6706017884533e6a74137918630a42db0058f622
lei jin <werewolf2101@msn.com>
2018-12-04 11:25:26 +0800

    model card 2

--- src/page/puzzlecards.js
+++ src/page/puzzlecards.js
@@ -1,46 +1,96 @@
+// import React, { Component } from 'react';
+// import { Card, Button } from 'antd';
+//
+// export default class PuzzleCardsPage extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.counter = 100;
+//         this.state = {
+//             cardList: [
+//                 {
+//                     id: 1,
+//                     setup: 'Did you hear about the two silk worms in a race?',
+//                     punchline: 'It ended in a tie',
+//                 },
+//                 {
+//                     id: 2,
+//                     setup: 'What happens to a frog\'s car when it breaks down?',
+//                     punchline: 'It gets toad away',
+//                 },
+//             ],
+//         }
+//     }
+//
+//     addNewCard = () => {
+//         this.setState(prevState => {
+//             const prevCardList = prevState.cardList;
+//             this.counter += 1;
+//             const card = {
+//                 id: this.counter,
+//                 setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+//                 punchline: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+//             };
+//             return {
+//                 cardList: prevCardList.concat(card),
+//             };
+//         });
+//     };
+//
+//     render() {
+//         return (
+//             <div>
+//                 {
+//                     this.state.cardList.map(card => {
+//                         return (
+//                             <Card key={card.id}>
+//                                 <div>Q: {card.setup}</div>
+//                                 <div>
+//                                     <strong>A: {card.punchline}</strong>
+//                                 </div>
+//                             </Card>
+//                         );
+//                     })
+//                 }
+//                 <div>
+//                     <Button onClick={this.addNewCard}> 添加卡片 </Button>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
 import React, { Component } from 'react';
-import { Card, Button } from 'antd';
+import { Card , Button } from 'antd';
+import { connect } from 'dva';
 
-export default class PuzzleCardsPage extends Component {
-    constructor(props) {
-        super(props);
-        this.counter = 100;
-        this.state = {
-            cardList: [
-                {
-                    id: 1,
-                    setup: 'Did you hear about the two silk worms in a race?',
-                    punchline: 'It ended in a tie',
-                },
-                {
-                    id: 2,
-                    setup: 'What happens to a frog\'s car when it breaks down?',
-                    punchline: 'It gets toad away',
-                },
-            ],
-        }
-    }
+const namespace = 'puzzlecards';
 
-    addNewCard = () => {
-        this.setState(prevState => {
-            const prevCardList = prevState.cardList;
-            this.counter += 1;
-            const card = {
-                id: this.counter,
-                setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
-                punchline: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            };
-            return {
-                cardList: prevCardList.concat(card),
-            };
-        });
+const mapStateToProps = (state) => {
+    const cardList = state[namespace].data;
+    return {
+        cardList,
     };
+};
 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onClickAdd: (newCard) => {
+            const action = {
+                type: `${namespace}/addNewCard`,
+                payload: newCard,
+            };
+            dispatch(action)
+        },
+    };
+};
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class PuzzleCardsPage extends Component {
     render() {
         return (
             <div>
                 {
-                    this.state.cardList.map(card => {
+                    this.props.cardList.map(card => {
                         return (
                             <Card key={card.id}>
                                 <div>Q: {card.setup}</div>
@@ -52,7 +102,10 @@ export default class PuzzleCardsPage extends Component {
                     })
                 }
                 <div>
-                    <Button onClick={this.addNewCard}> 添加卡片 </Button>
+                    <Button onClick={() => this.props.onClickAdd({
+                        setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                        punchline: 'here we use dva',
+                    })}> 添加卡片 </Button>
                 </div>
             </div>
         );
